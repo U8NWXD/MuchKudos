8a9a6b5870a4620a49ff0ecfdb17e88156f103b1
cs <cs.temporary@icloud.com>
2018-02-18 10:33:17 +0000

    Setup layout for viewing all kudos

---
+++ MuchKudos/AllKudosViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AllKudosViewController.swift
+//  MuchKudos
+//
+//  Created by cs on 2/18/18.
+//
+
+import UIKit
+import Firebase
+
+class AllKudosViewController: UIViewController {
+    @IBOutlet weak var allKudos_table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
